bf7cf6810cf059952a5c90506be434b6e3e73959
Sammontalbano22 <120127489+Sammontalbano22@users.noreply.github.com>
2025-05-04 20:29:31 -0600

    Update singlylinkedlist.py
    Clarifying the code.

--- singlylinkedlist.py
+++ singlylinkedlist.py
@@ -1,7 +1,71 @@
+"""
+
+Singly linked Lists
+--------------------
+A linked list is made up of nodes.
+Each node has two parts:
+
+Data – the actual value (e.g., a number, a name).
+
+Pointer (or Link) – a reference to the next node in the list.
+
+How it works:
+The list starts with a head pointer that points to the first node.
+
+Each node links to the next, forming a chain.
+
+The last node's pointer is set to null (or NULL in C), indicating the end of the list.
+
+
+
+
+
+
+#My remarks
+
+A singly linked list only allows traversal in one direction because each node points only to the next node, not the previous one. As a result, certain operations (like inserting or deleting at the head) are very efficient, while others (like accessing or removing elements at the end) require traversing the entire list and take more time.
+
+
+
+[Data | Next] → [Data | Next] → [Data | Next] → NULL 
+
+
+
+📊 Time Complexity Table
+Operation	Time Complexity
+add_first	O(1)
+add_last	O(n)
+get(index)	O(n)
+remove_first	O(1)
+remove_last	O(n)
+remove_at_index(i)	O(n)
+is_empty	O(1)
+get_size	O(1)
+
+
+
+
+"""
+
+
+
+
+
+
 import random
 
+
+
+
+
+
 class Node:
+'''
+This class constructs individual nodes for the sll
+'''
     def __init__(self, v, n):
+        creates a value(data) and a pointer(next)
+        [Data | Next]
         self.value = v
         self.next = n
     def __repr__(self):
@@ -10,6 +74,10 @@ class Node:
 class SinglyLinkedList:
     
     def __init__(self):
+        """   
+        Starts with a header node which will not store any data outside of the total size of the sll. *This is helpful for functions that require knowing the size.
+
+        """
         self.head = None
         self.size = 0
         
@@ -20,11 +88,14 @@ class SinglyLinkedList:
             return False
     
     def get_size(self)-> int:
+        """
+        A getter function for the stored size
+        """
         return self.size
         
         
     def add_first(self, v)->None:
-        new_node=Node(v,self.head)
+        new_node=Node(v,self.head)#Creates a new instance of node and adds it to the front of the sll.
         self.head=new_node
         self.size+=1
         
